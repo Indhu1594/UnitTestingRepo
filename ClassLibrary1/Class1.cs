67c70358f1529cb739f1a515a36c3bec043a61c8
Indhu1594 <indhu.muthuraj@gmail.com>
2022-10-29 00:16:25 -0400

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NUnitDemonstration
+{
+    public static class Triangle
+    {
+//Indhu's Test 
+        public static string ValidTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 210)
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+    }
+}
+//The next step that we need to perform is to add a new Project(named
---
+++ TestClass2/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnitDemonstration;
+
+namespace triangleclasstest
+{
+    public class Class1triangle
+    {
+        [Test]
+        public void validTriangle_Input60and60and60_OutputValidTriangle()
+        {
+//Indhu's Test
+            //Arrange
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 90;
+            //Act
+            string expected ="The triangle is valid.";
+            string actual=Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
