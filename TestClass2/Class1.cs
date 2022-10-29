efa5aa5976e08fdab2a42ceb2521dfce9ed718db
Indhu1594 <indhu.muthuraj@gmail.com>
2022-10-29 00:19:54 -0400

    Pushing changes to Repo

--- TestClass2/Class1.cs
+++ TestClass2/Class1.cs
@@ -17,7 +17,7 @@ namespace triangleclasstest
             //Arrange
             int firstAngle = 60;
             int secondAngle = 60;
-            int thirdAngle = 90;
+            int thirdAngle = 80;
             //Act
             string expected ="The triangle is valid.";
             string actual=Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
